4287590608ccfa469a9e084ecbb2c178dede840e
Philemon Kira <128330478+PhileKIRA@users.noreply.github.com>
2026-05-20 12:15:54 +1000

    Add files via upload

---
+++ public/muffin.php
@@ -0,0 +1,28 @@
+<?php
+require_once '../config/db.php';
+$pageTitle = 'Muffins';
+require_once '../public/header.php';
+include 'chatbot.php';
+
+$sql = "SELECT * FROM products p JOIN categories c ON p.category_id=c.category_id WHERE c.category_name='Muffins'";
+$result = $conn->query($sql);
+?>
+
+<div class="category-title">
+    <h2>Muffins</h2>
+</div>
+
+<div class="category-section">
+    <div class="product-grid">
+        <?php while($row = $result->fetch_assoc()): ?>
+        <div class="product-card">
+            <img src="/bakersbakery/<?php echo $row['image_path']; ?>">
+            <h3><?php echo $row['product_name']; ?></h3>
+            <p>K<?php echo $row['price']; ?></p>
+        </div>
+        <?php endwhile; ?>
+    </div>
+</div>
+
+
+<?php require_once '../public/footer.php'; ?>
